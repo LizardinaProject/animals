4312c20e029bb0a374593a66c534d1e8d6f462d9
LizardinaProject <lizawork2017@yandex.ru>
2022-12-07 13:30:09 +0300

    change seeder

--- database/seeds/AnimalKindSeeder.php
+++ database/seeds/AnimalKindSeeder.php
@@ -14,35 +14,35 @@ class AnimalKindSeeder extends Seeder
     {
         DB::table('animal_kinds')->insert([
             'kind' => 'Лысый кот',
-            'icon' => 'bold_cat.png',
-            'picture' => 'bold_cat_big.png',
-            'max_size' => '10',
+            'icon' => '/images/icons/bold_cat.png',
+            'picture' => '/images/pictures/bold_cat_big.png',
+            'max_size' => '150',
             'max_age' => '100',
-            'growth_factor' => '0.5',
+            'growth_factor' => '2',
         ]);
         DB::table('animal_kinds')->insert([
             'kind' => 'Собака',
-            'icon' => 'dog.png',
-            'picture' => 'dog_big.png',
-            'max_size' => '20',
+            'icon' => '/images/icons/dog.png',
+            'picture' => '/images/pictures/dog_big.png',
+            'max_size' => '200',
             'max_age' => '150',
             'growth_factor' => '1',
         ]);
         DB::table('animal_kinds')->insert([
             'kind' => 'Птица',
-            'icon' => 'bird.png',
-            'picture' => 'bird_big.png',
-            'max_size' => '8',
+            'icon' => '/images/icons/bird.png',
+            'picture' => '/images/pictures/bird_big.png',
+            'max_size' => '300',
             'max_age' => '100',
-            'growth_factor' => '0.5',
+            'growth_factor' => '2',
         ]);
         DB::table('animal_kinds')->insert([
             'kind' => 'Шерстяной кот',
-            'icon' => 'fluffy_cat.png',
-            'picture' => 'fluffy_cat_big.png',
-            'max_size' => '350',
+            'icon' => '/images/icons/fluffy_cat.png',
+            'picture' => '/images/pictures/fluffy_cat_big.png',
+            'max_size' => '400',
             'max_age' => '100',
-            'growth_factor' => '1.5',
+            'growth_factor' => '2',
         ]);
     }
 }
